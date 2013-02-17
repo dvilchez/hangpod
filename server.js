58ba3292aeed4fbfb4b318d755d9294fcc5f2bd5
David Vílchez Mendoza <dvilchez@Davids-MacBook-Pro.local>
2013-02-17 15:30:41 +0100

    process.env.PORT || 3000

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ app.get('/', function(req, res){
   res.send('Hello World');
 });
 
-app.listen(3000);
-console.log('Listening on port 3000');
+app.listen(process.env.PORT || 3000);
+console.log('Listening on port'+(process.env.PORT || 3000));
